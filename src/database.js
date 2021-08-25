e2676a572c2940f2b08cbdecac92c485623f1b75
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-25 06:51:06 +0530

    feat: database

---
+++ src/database.js
@@ -0,0 +1,193 @@
+const seriesLib = {
+  comedySeries: [
+    {
+      name: "Brooklyn Nine-Nine",
+      description:
+        "Comedy series following the exploits of Det. Jake Peralta and his diverse, lovable colleagues as they police the NYPD's 99th Precinct.",
+      rating: "8.4/10",
+      image: "imgs/brooklynNineNine.jpg"
+    },
+    {
+      name: "The Office",
+      description:
+        "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.",
+      rating: "8.9/10",
+      image: "imgs/theOffice.jpg"
+    },
+    {
+      name: "Friends",
+      description:
+        "Follows the personal and professional lives of six twenty to thirty-something-year-old friends living in Manhattan.",
+      rating: "8.9/10",
+      image: "imgs/friends.jpg"
+    },
+    {
+      name: "Never Have I Ever",
+      description:
+        "The complicated life of a modern-day first generation Indian American teenage girl, inspired by Mindy Kaling's own childhood.",
+      rating: "7.9/10",
+      image: "imgs/neverHaveIEver.jpg"
+    },
+    {
+      name: "The Umbrella Academy",
+      description:
+        "A family of former child heroes, now grown apart, must reunite to continue to protect the world.",
+      rating: "8.0/10",
+      image: "imgs/theUmbrellaAcademy.jpg"
+    }
+  ],
+
+  sciFiSeries: [
+    {
+      name: "Loki",
+      description:
+        "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
+      rating: "8.4/10",
+      image: "imgs/loki.jpg"
+    },
+    {
+      name: "Stranger Things",
+      description:
+        "When a young boy disappears, his mother, a police chief and his friends must confront terrifying supernatural forces in order to get him back.",
+      rating: "8.7/10",
+      image: "imgs/strangerThings.jpg"
+    },
+    {
+      name: "The Boys",
+      description:
+        "A group of vigilantes set out to take down corrupt superheroes who abuse their superpowers.",
+      rating: "8.7/10",
+      image: "imgs/theBoys.jpg"
+    },
+    {
+      name: "The Flash",
+      description:
+        "After being struck by lightning, Barry Allen wakes up from his coma to discover he's been given the power of super speed, becoming the next Flash, fighting crime in Central City.",
+      rating: "7.6/10",
+      image: "imgs/theFlash.jpg"
+    },
+    {
+      name: "Arrow",
+      description:
+        "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
+      rating: "7.5/10",
+      image: "imgs/arrow.jpg"
+    }
+  ],
+
+  romanceSeries: [
+    {
+      name: "Grey's Anatomy",
+      description:
+        "A drama centered on the personal and professional lives of five surgical interns and their supervisors.",
+      rating: "7.5/10",
+      image: "imgs/greysAnatomy.jpg"
+    },
+    {
+      name: "The Vampire Diaries",
+      description:
+        "The lives, loves, dangers and disasters in the town, Mystic Falls, Virginia. Creatures of unspeakable horror lurk beneath this town as a teenage girl is suddenly torn between two vampire brothers.",
+      rating: "7.7/10",
+      image: "imgs/theVampireDiaries.jpg"
+    },
+    {
+      name: "You",
+      description:
+        "A dangerously charming, intensely obsessive young man goes to extreme measures to insert himself into the lives of those he is transfixed by.",
+      rating: "7.7/10",
+      image: "imgs/you.jpg"
+    },
+    {
+      name: "How I Met Your Mother",
+      description:
+        "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
+      rating: "8.3/10",
+      image: "imgs/howIMetYourMother.jpg"
+    },
+    {
+      name: "The Big Bang Theory",
+      description:
+        "A woman who moves into an apartment across the hall from two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory.",
+      rating: "8.1/10",
+      image: "imgs/theBigBangTheory.jpg"
+    }
+  ],
+
+  actionSeries: [
+    {
+      name: "Game of Thrones",
+      description:
+        "Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.",
+      rating: "9.2/10",
+      image: "imgs/gameOfThrones.jpg"
+    },
+    {
+      name: "La Casa De Papel",
+      description:
+        "An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.",
+      rating: "8.3/10",
+      image: "imgs/moneyHeist.jpg"
+    },
+    {
+      name: "Prison Break",
+      description:
+        "Due to a political conspiracy, an innocent man is sent to death row and his only hope is his brother, who makes it his mission to deliberately get himself sent to the same prison in order to break the both of them out, from the inside.",
+      rating: "8.3/10",
+      image: "imgs/prisonBreak.jpg"
+    },
+    {
+      name: "The Falcon and the Winter Soldier",
+      description:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.",
+      rating: "7.3/10",
+      image: "imgs/theFalconAndTheWinterSoldier.jpg"
+    },
+    {
+      name: "Killing Eve",
+      description:
+        "After a series of events, the lives of a security operative and an assassin become inextricably linked.",
+      rating: "8.2/10",
+      image: "imgs/killingEve.jpg"
+    }
+  ],
+
+  crimeSeries: [
+    {
+      name: "Breaking Bad",
+      description:
+        "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+      rating: "9.4/10",
+      image: "imgs/breakingBad.jpg"
+    },
+    {
+      name: "Lucifer",
+      description:
+        "Lucifer Morningstar has decided he's had enough of being the dutiful servant in Hell and decides to spend some time on Earth to better understand humanity. He settles in Los Angeles - the City of Angels.",
+      rating: "8.1/10",
+      image: "imgs/lucifer.jpg"
+    },
+    {
+      name: "Peaky Blinders",
+      description:
+        "A gangster family epic set in 1900s England, centering on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby.",
+      rating: "8.8/10",
+      image: "imgs/peakyBlinders.jpg"
+    },
+    {
+      name: "Dark",
+      description:
+        "A family saga with a supernatural twist, set in a German town, where the disappearance of two young children exposes the relationships among four families.",
+      rating: "8.8/10",
+      image: "imgs/dark.jpg"
+    },
+    {
+      name: "Mr. Robot",
+      description:
+        "Elliot, a brilliant but highly unstable young cyber-security engineer and vigilante hacker, becomes a key figure in a complex game of global dominance when he and his shadowy allies try to take down the corrupt corporation he works for.",
+      rating: "8.5/10",
+      image: "imgs/mrRobot.jpg"
+    }
+  ]
+};
+
+export default seriesLib;
