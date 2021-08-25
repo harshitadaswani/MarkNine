fae0692b7b6b619c0b4ccc57197cb38a5773c822
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-25 07:53:49 +0530

    logic built

--- src/App.js
+++ src/App.js
@@ -2,11 +2,60 @@ import React, { useState } from "react";
 import "./styles.css";
 import seriesLib from "./database.js";
 
+const genres = Object.keys(seriesLib);
+
 export default function App() {
+  const [currentGenre, setGenre] = useState(seriesLib.comedySeries);
+
+  function genreClickHandler(genre) {
+    const series = seriesLib[genre];
+    setGenre(series);
+  }
+
+  function SeriesPost(series) {
+    const title = seriesLib.name;
+    const imgUrl = seriesLib.image;
+    const desc = seriesLib.description;
+    const rating = seriesLib.rating;
+    const seriesList = (
+      <li>
+        <img alt="poster" src={imgUrl} />
+        <div style={{ padding: "0rem 1rem" }}>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+          <p style={{ fontWeight: "bold" }}>{rating}</p>
+        </div>
+      </li>
+    );
+    return seriesList;
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Seriopolis</h1>
+      <h2>
+        Explore the Top English WebSeries curated just for you. Select a genre
+        to get started.
+      </h2>
+      <div>
+        {genres.map((genre) => {
+          return (
+            <button
+              key={genre}
+              className="btn"
+              onClick={() => genreClickHandler(genre)}
+            >
+              {genre}
+            </button>
+          );
+        })}
+      </div>
+      <hr />
+      <ul>
+        {currentGenre.map((series) => {
+          return SeriesPost(series);
+        })}
+      </ul>
     </div>
   );
 }
--- src/database.js
+++ src/database.js
@@ -5,35 +5,35 @@ const seriesLib = {
       description:
         "Comedy series following the exploits of Det. Jake Peralta and his diverse, lovable colleagues as they police the NYPD's 99th Precinct.",
       rating: "8.4/10",
-      image: "imgs/brooklynNineNine.jpg"
+      image: "https://i.postimg.cc/Sx3gvPvH/brooklyn-Nine-Nine.jpg"
     },
     {
       name: "The Office",
       description:
         "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.",
       rating: "8.9/10",
-      image: "imgs/theOffice.jpg"
+      image: "https://i.postimg.cc/2ykGWN1L/the-Office.jpg"
     },
     {
       name: "Friends",
       description:
         "Follows the personal and professional lives of six twenty to thirty-something-year-old friends living in Manhattan.",
       rating: "8.9/10",
-      image: "imgs/friends.jpg"
+      image: "https://i.postimg.cc/JhtWbSBg/friends.jpg"
     },
     {
       name: "Never Have I Ever",
       description:
         "The complicated life of a modern-day first generation Indian American teenage girl, inspired by Mindy Kaling's own childhood.",
       rating: "7.9/10",
-      image: "imgs/neverHaveIEver.jpg"
+      image: "https://i.postimg.cc/9FztHpZ2/never-Have-IEver.jpg"
     },
     {
       name: "The Umbrella Academy",
       description:
         "A family of former child heroes, now grown apart, must reunite to continue to protect the world.",
       rating: "8.0/10",
-      image: "imgs/theUmbrellaAcademy.jpg"
+      image: "https://i.postimg.cc/V6YnGBm3/the-Umbrella-Academy.jpg"
     }
   ],
 
@@ -43,35 +43,35 @@ const seriesLib = {
       description:
         "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
       rating: "8.4/10",
-      image: "imgs/loki.jpg"
+      image: "https://i.postimg.cc/Ss83d4QZ/loki.jpg"
     },
     {
       name: "Stranger Things",
       description:
         "When a young boy disappears, his mother, a police chief and his friends must confront terrifying supernatural forces in order to get him back.",
       rating: "8.7/10",
-      image: "imgs/strangerThings.jpg"
+      image: "https://i.postimg.cc/VN9XWHQq/stranger-Things.jpg"
     },
     {
       name: "The Boys",
       description:
         "A group of vigilantes set out to take down corrupt superheroes who abuse their superpowers.",
       rating: "8.7/10",
-      image: "imgs/theBoys.jpg"
+      image: "https://i.postimg.cc/Cx34NqZg/theBoys.jpg"
     },
     {
       name: "The Flash",
       description:
         "After being struck by lightning, Barry Allen wakes up from his coma to discover he's been given the power of super speed, becoming the next Flash, fighting crime in Central City.",
       rating: "7.6/10",
-      image: "imgs/theFlash.jpg"
+      image: "https://i.postimg.cc/j2MXQ3fg/theFlash.jpg"
     },
     {
       name: "Arrow",
       description:
         "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
       rating: "7.5/10",
-      image: "imgs/arrow.jpg"
+      image: "https://i.postimg.cc/B6rsZNbz/arrow.jpg"
     }
   ],
 
@@ -81,35 +81,35 @@ const seriesLib = {
       description:
         "A drama centered on the personal and professional lives of five surgical interns and their supervisors.",
       rating: "7.5/10",
-      image: "imgs/greysAnatomy.jpg"
+      image: "https://i.postimg.cc/rwfkRqzx/greys-Anatomy.jpg"
     },
     {
       name: "The Vampire Diaries",
       description:
         "The lives, loves, dangers and disasters in the town, Mystic Falls, Virginia. Creatures of unspeakable horror lurk beneath this town as a teenage girl is suddenly torn between two vampire brothers.",
       rating: "7.7/10",
-      image: "imgs/theVampireDiaries.jpg"
+      image: "https://i.postimg.cc/Wp8XvzyG/the-Vampire-Diaries.jpg"
     },
     {
       name: "You",
       description:
         "A dangerously charming, intensely obsessive young man goes to extreme measures to insert himself into the lives of those he is transfixed by.",
       rating: "7.7/10",
-      image: "imgs/you.jpg"
+      image: "https://i.postimg.cc/d14dzk0D/you.jpg"
     },
     {
       name: "How I Met Your Mother",
       description:
         "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
       rating: "8.3/10",
-      image: "imgs/howIMetYourMother.jpg"
+      image: "https://i.postimg.cc/8PBDDRSy/how-IMet-Your-Mother.jpg"
     },
     {
       name: "The Big Bang Theory",
       description:
         "A woman who moves into an apartment across the hall from two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory.",
       rating: "8.1/10",
-      image: "imgs/theBigBangTheory.jpg"
+      image: "https://i.postimg.cc/rFhnSsCV/the-Big-Bang-Theory.jpg"
     }
   ],
 
@@ -119,35 +119,36 @@ const seriesLib = {
       description:
         "Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.",
       rating: "9.2/10",
-      image: "imgs/gameOfThrones.jpg"
+      image: "https://i.postimg.cc/prJ7NBFz/game-Of-Thrones.jpg"
     },
     {
       name: "La Casa De Papel",
       description:
         "An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.",
       rating: "8.3/10",
-      image: "imgs/moneyHeist.jpg"
+      image: "https://i.postimg.cc/7hr8r6Q8/money-Heist.jpg"
     },
     {
       name: "Prison Break",
       description:
         "Due to a political conspiracy, an innocent man is sent to death row and his only hope is his brother, who makes it his mission to deliberately get himself sent to the same prison in order to break the both of them out, from the inside.",
       rating: "8.3/10",
-      image: "imgs/prisonBreak.jpg"
+      image: "https://i.postimg.cc/LsPxKTd5/prison-Break.jpg"
     },
     {
       name: "The Falcon and the Winter Soldier",
       description:
         "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.",
       rating: "7.3/10",
-      image: "imgs/theFalconAndTheWinterSoldier.jpg"
+      image:
+        "https://i.postimg.cc/J4g3rk5k/the-Falcon-And-The-Winter-Soldier.jpg"
     },
     {
       name: "Killing Eve",
       description:
         "After a series of events, the lives of a security operative and an assassin become inextricably linked.",
       rating: "8.2/10",
-      image: "imgs/killingEve.jpg"
+      image: "https://i.postimg.cc/sXhdLSm4/killing-Eve.jpg"
     }
   ],
 
@@ -157,35 +158,35 @@ const seriesLib = {
       description:
         "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
       rating: "9.4/10",
-      image: "imgs/breakingBad.jpg"
+      image: "https://i.postimg.cc/7ZH4L5Pq/breaking-Bad.jpg"
     },
     {
       name: "Lucifer",
       description:
         "Lucifer Morningstar has decided he's had enough of being the dutiful servant in Hell and decides to spend some time on Earth to better understand humanity. He settles in Los Angeles - the City of Angels.",
       rating: "8.1/10",
-      image: "imgs/lucifer.jpg"
+      image: "https://i.postimg.cc/cLrDTWXQ/lucifer.jpg"
     },
     {
       name: "Peaky Blinders",
       description:
         "A gangster family epic set in 1900s England, centering on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby.",
       rating: "8.8/10",
-      image: "imgs/peakyBlinders.jpg"
+      image: "https://i.postimg.cc/JhCbJCg7/peaky-Blinders.jpg"
     },
     {
       name: "Dark",
       description:
         "A family saga with a supernatural twist, set in a German town, where the disappearance of two young children exposes the relationships among four families.",
       rating: "8.8/10",
-      image: "imgs/dark.jpg"
+      image: "https://i.postimg.cc/c1FMzjKT/dark.jpg"
     },
     {
       name: "Mr. Robot",
       description:
         "Elliot, a brilliant but highly unstable young cyber-security engineer and vigilante hacker, becomes a key figure in a complex game of global dominance when he and his shadowy allies try to take down the corrupt corporation he works for.",
       rating: "8.5/10",
-      image: "imgs/mrRobot.jpg"
+      image: "https://i.postimg.cc/fTK41Mcp/mrRobot.jpg"
     }
   ]
 };
